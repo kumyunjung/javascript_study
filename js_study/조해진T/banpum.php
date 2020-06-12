6e64622b133fd11a6e346d53c6c5c965f49d52bb
kumyunjung <yuunjuun@gmail.com>
2020-06-12 13:33:02 +0900

    고객상담페이지

---
+++ js_study/조해진T/banpum.php
@@ -0,0 +1,7 @@
+<?
+echo "name:". $_POST["name"]. "<br>";
+echo "agree:". $_POST["agree"]. "<br>";
+echo "email:". $_POST["email01"]. "@". $_POST["email02"]. "<br>";
+echo "subject:". $_POST["subject"]. "<br>";
+echo "content:". $_POST["content"]. "<br>";
+?>
