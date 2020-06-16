308904cd79ebc35c95772176c58af58aba6cb01d
kumyunjung <yuunjuun@gmail.com>
2020-06-16 15:07:07 +0900

    반품페이지php

--- 조해진T/banpum.php
+++ 조해진T/banpum.php
@@ -3,5 +3,5 @@ echo "name:". $_POST["name"]. "<br>";
 echo "agree:". $_POST["agree"]. "<br>";
 echo "email:". $_POST["email01"]. "@". $_POST["email02"]. "<br>";
 echo "subject:". $_POST["subject"]. "<br>";
-echo "content:". $_POST["content"]. "<br>";
+echo "content:". $_POST["content"]. "<br>"; 
 ?>
