5ac2bddd4078db7f7329afea45f2ef15cc97838b
kumyunjung <yuunjuun@gmail.com>
2020-07-13 12:03:08 +0900

    express server

--- 이준우T/nodejs/server/app.js
+++ 이준우T/nodejs/server/app.js
@@ -1,1 +1,26 @@
-console.log('hellod');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5003;
+
+app.set('views', `${__dirname}/public`)
+app.set('view engine', 'html')
+app.engine('html', require('ejs').renderFile)
+app.use(express.static('src', {
+    extensions: ['html']
+}))
+
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.get('/api/users', (req, res) => {
+    const users = [{
+        name: 'aa',
+        age: 20
+    }]
+    res.send(users)
+})
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+})
