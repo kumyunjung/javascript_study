869ae844b6382a3d6ccf801c439e7cf33b12fd73
kumyunjung <yuunjuun@gmail.com>
2020-07-13 16:06:14 +0900

    chatting

--- 이준우T/nodejs/server/app.js
+++ 이준우T/nodejs/server/app.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 5000;
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.set('views', `${__dirname}/public`)
 app.set('view engine', 'html')
 app.engine('html', require('ejs').renderFile)
-app.use(express.static('src', {
+app.use(express.static('public', {
     extensions: ['html']
 }))
 
-
+io.on('connection', (socket) => {
+    console.log('User connected..');
+    socket.on('chatting', (data) => {
+        io.sockets.emit('chatting', data)
+    })
+})
 
 app.get('/', (req, res) => {
+    console.log(__dirname);
     res.render('index')
 })
 app.get('/api/users', (req, res) => {
@@ -21,6 +29,8 @@ app.get('/api/users', (req, res) => {
     }]
     res.send(users)
 })
-app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`);
-})
+// app.listen(PORT, () => {
+//     console.log(`server is running on ${PORT}`);
+// })
+http.listen(PORT, () =>
+    console.log(`listening on localhost:${PORT}`));
