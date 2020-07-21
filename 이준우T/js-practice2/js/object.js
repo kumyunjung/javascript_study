da3c8ba464f35cc6791af5cbc13b7d35c73d5147
kumyunjung <yuunjuun@gmail.com>
2020-07-21 18:41:56 +0900

    object

---
+++ 이준우T/js-practice2/js/object.js
@@ -0,0 +1,21 @@
+// const name = 'jade'
+// const person = {
+//     name: 'jade',
+//     hobby: ['농구', '축구', '야구']
+
+// }
+// person.name = 'john'
+// person['name'] = 'mike'
+// person.hobby[2] = '배구'
+
+// console.log(person.hobby[2]);
+
+const city = {
+    name: 'Daegu',
+    population: 100,
+    area: 100,
+    isHOT: true,
+    food: ['막창', '막창', '막창']
+}
+
+console.log(city);
