b740c0f2e0ea5dda012ad44037b7a9594003773c
kumyunjung <yuunjuun@gmail.com>
2020-07-21 19:31:23 +0900

    function

---
+++ 이준우T/js-practice2/js/function.js
@@ -0,0 +1,12 @@
+// function sum(a, b, c) {
+//     console.log(a * b / c);
+// }
+// sum(30, 20, 10)
+
+const circle = document.querySelector('.circle')
+
+
+function change(color) {
+    circle.style.background = color
+}
+change('red')
